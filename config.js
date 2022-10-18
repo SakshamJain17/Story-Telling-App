31b94f03e07d484f899bf7972f990f735d3ae7b1
SakshamJain17 <71916941+SakshamJain17@users.noreply.github.com>
2022-10-18 14:35:07 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export var firebaseConfig = {
+  apiKey: "AIzaSyDDLH5VXHJGMCBuoqIlFqoxq0XNAI49Cig",
+  authDomain: "story-telling-app-1f6c9.firebaseapp.com",
+  databaseURL: "https://story-telling-app-1f6c9-default-rtdb.firebaseio.com",
+  projectId: "story-telling-app-1f6c9",
+  storageBucket: "story-telling-app-1f6c9.appspot.com",
+  messagingSenderId: "989879859384",
+  appId: "1:989879859384:web:9420d7eae0d949aed29c4d"
+};
